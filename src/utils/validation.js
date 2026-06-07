821efc633c2bf95e564de8af9cbab383355e6b64
amiyasingh <23ucc513@lnmiit.ac.in>
2026-06-07 14:28:02 +0530

    login API

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,18 +1,16 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, lastName, emailId, password } = req.body;
-    if(!firstName || !lastName){
-        throw new Error("Name is required to signup");
-    }
-    else if(!validator.isEmail(emailId)){
-        throw new Error("Email is not valid");
-    }
-    else if(!validator.isStrongPassword(password)) {
-        throw new Error("Password is not strong enough");
-    }
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is required to signup");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough");
+  }
 };
 
 module.exports = {
-    validateSignUpData
-}
+  validateSignUpData,
+};
