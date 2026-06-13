58bb7f2c524fa22a48f27dce254468631488e176
amiyasingh <23ucc513@lnmiit.ac.in>
2026-06-13 21:53:30 +0530

    PATCH /profile/password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,9 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require('validator');
+
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -28,5 +31,29 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.status(400).send("ERROR: " + err.message);
   }
 });
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try{
+    const { password } = req.body;
+    if(!password) throw new Error("Password is required");
+
+    if(!validator.isStrongPassword(password)){
+      throw new Error("Password is not strong enough")
+    }
+    const loggedInUser = req.user;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+
+    res.send({
+      message: "Password Updated Successfully",
+      data: loggedInUser,
+    });
+
+  } catch (err) {
+    res.status(400).send("ERROR: "+err.message);
+  }
+})
 
 module.exports = profileRouter;
